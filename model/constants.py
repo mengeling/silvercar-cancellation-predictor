f39b5ac13e13c64cbd8033c0d857e1e025d62fcb
Michael Engeling <michael.engeling@gmail.com>
2018-01-11 21:11:28 -0600

    Created pipeline

---
+++ model/constants.py
@@ -0,0 +1,37 @@
+ENGINE = "postgresql://mengeling:mengeling@localhost:5432/silvercar"
+
+PAST_RESERVATIONS = (
+    "SELECT r.user_id, r.current_state, r.created_as_guest, r.local_rental, "
+    "r.awards_referral_bonus, r.pickup, r.dropoff, r.created_at, r.updated_at, r.promo_code_id, "
+    "r.booking_application, r.reservation_frequency, l.time_zone "
+    "FROM reservations r "
+    "LEFT JOIN locations l ON r.pickup_location_id = l.id "
+    "WHERE current_state != 'booked' AND current_state != 'pending_agreement'"
+)
+
+BOOKED_RESERVATIONS = (
+    "SELECT r.user_id, r.current_state, r.created_as_guest, r.local_rental, "
+    "r.awards_referral_bonus, r.pickup, r.dropoff, r.created_at, r.updated_at, r.promo_code_id, "
+    "r.booking_application, r.reservation_frequency, l.time_zone "
+    "FROM reservations r "
+    "LEFT JOIN locations l ON r.pickup_location_id = l.id "
+    "WHERE current_state = 'booked'"
+)
+
+USERS = (
+    "SELECT u.id, u.is_gds_user, u.referral_code, i.is_corporate, i.is_personal, i.is_silvercar, c.postal_code "
+    "FROM users u "
+    "LEFT JOIN insurance i ON u.id = i.user_id "
+    "LEFT JOIN user_profile p ON u.id = p.user_id "
+    "LEFT JOIN credit_cards c ON p.id = c.user_profile_id"
+)
+
+DATE_FEATURES_TO_DROP = ["pickup", "dropoff", "created_at", "updated_at"]
+
+WEB_APPS = ["web", "web-desktop", "web-mobile", "web-tablet"]
+
+INSURANCE_DICT = {"is_corporate": "insurance_corp", "is_silvercar": "insurance_silvercar",
+                  "is_personal": "insurance_personal"}
+
+OTHER_FEATURES_TO_DROP = ["promo_code_id", "booking_application", "reservation_frequency", "referral_code",
+                          "postal_code", "time_zone"]
--- model/model.py
+++ model/model.py
@@ -1,54 +1,104 @@
-import pickle
+import numpy as np
 import pandas as pd
-from sklearn.base import BaseEstimator, TransformerMixin
+from sqlalchemy import create_engine
+from collections import defaultdict
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
+
+import constants as C
 
 
-def prepare_data(df):
-    df = pd.get_dummies(df)
-    y = df.pop("current_state").values
-    X = df.values
-    return X, y
+class Pipeline():
+    def __init__(self):
+        """
+        Initialize model
+        """
+        self.df = None
+        self.X = None
+        self.X_booked = None
+        self.y = None
+        self.d = defaultdict(list)
+        self.scaler = StandardScaler()
+
+    def fit(self, df, y):
+        self.df = df
+        self.y = y
+        self._create_date_features()
+        self._create_binary_features()
+        self._create_historical_features()
+        self.X = self.scaler.fit_transform(self.df.values)
+
+    def transform(self, df_booked):
+        self.df = df_booked
+        self._create_date_features()
+        self._create_binary_features()
+        self._create_historical_features(booked=True)
+        self.df.drop("current_state", axis=1, inplace=True)
+        self.X_booked = self.scaler.fit_transform(self.df.values)
+
+    def _create_date_features(self):
+        self._change_datetimes("pickup", "dropoff", "created_at", "updated_at")
+        self._calculate_time_between(days_to_pickup=("pickup", "created_at"), trip_duration=("dropoff", "pickup"),
+                                     updated_time=("updated_at", "created_at"))
+        self.df["weekend_pickup"] = self.df["pickup"].dt.dayofweek.isin([4, 5, 6]).astype(int)
+        self.df["winter_pickup"] = self.df["pickup"].dt.month.isin([1, 12]).astype(int)
+        self.df["updated_reservation"] = (self.df["updated_at"].dt.date > self.df["created_at"].dt.date).astype(int)
+        self.df.drop(C.DATE_FEATURES_TO_DROP, axis=1, inplace=True)
+
+    def _create_binary_features(self):
+        self.df["used_promo"] = self.df["promo_code_id"].notnull().astype(int)
+        self.df["web_booking"] = (self.df["booking_application"].isin(C.WEB_APPS)).astype(int)
+        self.df["new_customer"] = (self.df["reservation_frequency"] != "repeat_customer").astype(int)
+        self.df["used_referral"] = self.df["referral_code"].notnull().astype(int)
+        self.df["credit_card"] = self.df["postal_code"].notnull().astype(int)
+        self.df["western_pickup"] = ((self.df["time_zone"] == "pst") | (self.df["time_zone"] == "mst")).astype(int)
+        self.df.fillna(value={"is_corporate": 0, "is_silvercar": 0, "is_personal": 0, "is_gds_user": 0}, inplace=True)
+        self.df.rename(columns=C.INSURANCE_DICT, inplace=True)
+        self.df.drop(C.OTHER_FEATURES_TO_DROP, axis=1, inplace=True)
+
+    def _change_datetimes(self, *args):
+        for col_name in args:
+            self.df[col_name] = pd.to_datetime('1899-12-30') + pd.to_timedelta(self.df[col_name], 'D')
+
+    def _calculate_time_between(self, **kwargs):
+        for k, (v1, v2) in kwargs.items():
+            self.df[k] = (self.df[v1] - self.df[v2]).dt.total_seconds() / 86400
+
+    def _create_historical_features(self, booked=False):
+        self._get_past_ride_cnt(booked)
+        self.df["rides"] = self.ride_history
+        self.df["past_rides"] = self.df["rides"].apply(lambda lst: len(lst))
+        self.df["past_cancellations"] = self.df["rides"].apply(lambda lst: sum(lst))
+        self.df["past_percent_cancelled"] = self.df["past_cancellations"] / self.df["past_rides"]
+        self.df["past_percent_cancelled"] = self.df["past_percent_cancelled"].fillna(self.y.mean())
+        self.df.drop(["user_id", "rides"], axis=1, inplace=True)
+
+    def _get_past_ride_cnt(self, booked):
+        self.ride_history = []
+        for i, user_id in enumerate(self.df["user_id"]):
+            self.ride_history.append(self.d[user_id].copy())
+            if not booked:
+                self.d[user_id].append(self.y[i])
 
 
-def merge_data(df1, df2):
-    df = df1.join(df2, how="left", on="user_id")
-    df.drop("user_id", axis=1, inplace=True)
-    return df.dropna()
-
-
-def load_users(filename):
-    df = pd.read_csv(filename)
-    return df[["id", "sign_in_count"]].set_index("id")
-
-
-def get_datetime(series):
-    return pd.to_datetime('1899-12-30') + pd.to_timedelta(series, 'D')
-
-
-def calculate_datetime_difference(series1, series2):
-    return (get_datetime(series1) - get_datetime(series2)).dt.total_seconds() / 86400
-
-
-def load_reservations(filename):
-    df = pd.read_csv(filename)
-    df["days_to_pickup"] = calculate_datetime_difference(df["pickup"], df["created_at"])
-    df["used_promo"] = (df["promo_code_id"].notnull()).astype(int)
-    df["current_state"] = df["current_state"].map({"cancelled": 1, "finished": 0})
-    return df[["user_id", "current_state", "days_to_pickup", "reservation_frequency", "used_promo"]]
-
-
-def get_data():
-    df_reservations = load_reservations('../data/silvercar_reservations.csv')
-    df_users = load_users('../data/silvercar_users.csv')
-    return merge_data(df_reservations, df_users)
+def get_data(booked=False):
+    """
+    Query Postgres database to get all of the features needed for the model
+    """
+    engine = create_engine(C.ENGINE)
+    query = C.BOOKED_RESERVATIONS if booked else C.PAST_RESERVATIONS
+    df_reservations = pd.read_sql_query(query, con=engine)
+    df_users = pd.read_sql_query(C.USERS, con=engine)
+    df_users = df_users[~df_users.index.duplicated(keep='first')]
+    return df_reservations.join(df_users.set_index("id"), on="user_id", how="left")
 
 
 if __name__ == '__main__':
     df = get_data()
-    X, y = prepare_data(df)
-    lr = LogisticRegression()
-    lr.fit(X, y)
-    with open('model.pkl', 'wb') as f:
-        pickle.dump(lr, f)
+    df["current_state"] = ((df["current_state"] != "finished") & (df["current_state"] != "started")).astype(int)
+    y = df.pop("current_state").values
+    pipeline = Pipeline()
+    pipeline.fit(df, y)
+    df_booked = get_data(booked=True)
+    pipeline.transform(df_booked)
