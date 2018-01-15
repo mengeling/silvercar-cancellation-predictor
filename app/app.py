99feadd15d0f796efa0b8db3a12e0a3846731459
Michael Engeling <michael.engeling@gmail.com>
2018-01-14 21:23:15 -0600

    Completed functionality on new reservation page

--- app/app.py
+++ app/app.py
@@ -23,25 +23,11 @@ def index():
 
 @app.route('/new-reservation/')
 def new_reservation():
-    return render_template('new_reservation.html')
-
-# Cancelled average = 0.4112255607631243
+    return render_template('new_reservation.html', locations=np.sort(df["name"].unique()))
 
 
-# @app.route('/new-reservation/', methods=['GET', 'POST'])
-# def new_reservation():
-#     if request.method == 'POST':
-#         probability = request.form['text']
-#         probability = 0.8
-#         return render_template('new_reservation.html', probability=probability, prediction=None)
-#     text = np.array([request.form['text']])
-#     probability = model.predict_proba(text)[0]
-#     prediction = probability > C.THRESHOLD
-#     return render_template('new_reservation.html', probability=None, prediction=None)
-
-
-@app.route('/get_subset/', methods=['GET'])
-def get_subset():
+@app.route('/get_df_subset/', methods=['GET'])
+def get_df_subset():
     location = request.args.get('location', None)
     month = request.args.get('month', None)
     if location is not None and month is not None:
@@ -55,6 +41,38 @@ def get_subset():
             "data": df_subset.to_html(index=False)
         }
         return jsonify(d)
+
+
+@app.route('/calculate_probability/', methods=['GET'])
+def calculate_probability():
+    df_new = pd.DataFrame(request.args, index=[0])
+    df_new = df_new.apply(pd.to_numeric, errors='ignore')
+    df_new["past_percent_cancelled"] = df_new["past_cancellations"] / df_new["past_rides"]\
+        if df_new["past_rides"].sum() != 0 else 0.41
+    time_zone = df[df["name"] == df_new["location"].iloc[0]]["time_zone"].iloc[0]
+    df_new["western_pickup"] = (time_zone == "pst") | (time_zone == "mst")
+    df_new["insurance_corporate"] = df_new["insurance"].iloc[0] == "Corporate"
+    df_new["insurance_personal"] = df_new["insurance"].iloc[0] == "Personal"
+    df_new["insurance_silvercar"] = df_new["insurance"].iloc[0] == "Silvercar"
+    df_new["created_at"] = pd.to_datetime(df_new["created_at"])
+    df_new["pickup"] = pd.to_datetime(df_new["pickup"])
+    df_new["dropoff"] = pd.to_datetime(df_new["dropoff"])
+    df_new["days_to_pickup"] = (df_new["pickup"] - df_new["created_at"]).dt.total_seconds() / 86400
+    df_new["trip_duration"] = (df_new["dropoff"] - df_new["pickup"]).dt.total_seconds() / 86400
+    df_new["weekend_pickup"] = df_new["pickup"].dt.dayofweek.isin([4, 5, 6])
+    df_new["winter_pickup"] = df_new["pickup"].dt.month.isin([1, 12])
+    df_new.replace({"Yes": 1, "No": 0, True: 1, False: 0}, inplace=True)
+    df_new = df_new[C.FEATURES_TO_KEEP]
+    X = model.pipeline.scaler.transform(df_new)
+    probability = model.classifier.predict_proba(X)[0, 1]
+    prediction = "Cancelled Ride" if probability > C.THRESHOLD else "Finished Ride"
+    price = df_new["trip_duration"].iloc[0] * 50
+    d = {
+        "probability": "{:,.2f}".format(probability),
+        "prediction": prediction,
+        "price": "${:,.0f}".format(price)
+    }
+    return jsonify(d)
 
 
 if __name__ == '__main__':
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,18 +1,50 @@
-$(document).ready(function() {
-  $( "select" ).change(function () {
-    $.ajax({
-      type: "GET",
-      url: $SCRIPT_ROOT + "/get_subset/",
-      contentType: "application/json; charset=utf-8",
-      data: { location: $( "select.select-location option:selected" ).val(),
-              month: $( "select.select-month option:selected" ).val()},
-      success: function(data) {
-        $('.header').text(data.location + " Reservations");
-        $('.total-count').text("Current Reservations: " + data.total_count);
-        $('.cancel_count').text("Predicted Cancellations: " + data.cancel_count);
-        $('.revenue').text("Predicted Revenue: " + data.revenue);
-        $('.data').html(data.data);
-      }
-    });
-  }).change();
-});
+$( "select.select" ).change(function () {
+  $.ajax({
+    type: "GET",
+    url: $SCRIPT_ROOT + "/get_df_subset/",
+    contentType: "application/json; charset=utf-8",
+    data: { location: $( "select.select-location option:selected" ).val(),
+            month: $( "select.select-month option:selected" ).val() },
+    success: function(data) {
+      $('.header').text(data.location + " Reservations");
+      $('.total-count').text("Current Reservations: " + data.total_count);
+      $('.cancel_count').text("Predicted Cancellations: " + data.cancel_count);
+      $('.revenue').text("Predicted Revenue: " + data.revenue);
+      $('.data').html(data.data);
+    }
+  });
+})
+.change();
+
+
+$( ".form-input" ).change(function () {
+  var data =
+  console.log( data );
+  $.ajax({
+    type: "GET",
+    url: $SCRIPT_ROOT + "/calculate_probability/",
+    contentType: "application/json; charset=utf-8",
+    data: { created_as_guest: $( "input[name='guest']:checked" ).val(),
+            local_rental: $( "input[name='local']:checked" ).val(),
+            awards_referral_bonus: $( "input[name='referral-bonus']:checked" ).val(),
+            is_gds_user: $( "input[name='gds']:checked" ).val(),
+            insurance: $( "input[name='insurance']:checked" ).val(),
+            created_at: $( "input[name='created']" ).val(),
+            pickup: $( "input[name='pickup']" ).val(),
+            dropoff: $( "input[name='dropoff']" ).val(),
+            used_promo: $( "input[name='promo']:checked" ).val(),
+            used_referral: $( "input[name='referred']:checked" ).val(),
+            credit_card: $( "input[name='credit-card']:checked" ).val(),
+            web_booking: $( "input[name='web']:checked" ).val(),
+            new_customer: $( "input[name='new-customer']:checked" ).val(),
+            location: $( "select option:selected" ).val(),
+            past_rides: $( "input[name='ride-count']" ).val(),
+            past_cancellations: $( "input[name='cancel-count']" ).val() },
+    success: function(data) {
+      $('.probability').text("Probability: " + data.probability);
+      $('.prediction').text("Prediction: " + data.prediction);
+      $('.price').text("Price: " + data.price);
+    }
+  });
+})
+.change();
